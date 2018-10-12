e70855cfd7196badf4c2580bf5965d7e78ef90b0
Nathan <nathaninbar@gmail.com>
2018-10-11 22:36:49 -0700

    working on trade logic
    uninstall: (oauth2client, PyOpenSSL, gspread)<-google spreadsheets
    
    install: pandas
    ---
    moved itemid dictionary into market manager
    
    added convertToChaos
    
    NOTE: need to think of a way to remove outliers from the list, if outlier is removed add another to the list,
    
    psuedo table that will be stored in a csv
    https://docs.google.com/spreadsheets/d/1P7ZMO-8Szx_nTOHMPZWW1SFwllcDkdOBOzfswHj0Dcw/edit?usp=sharing

--- main/main.py
+++ main/main.py
@@ -3,7 +3,31 @@ from toolbox.chat import *
 from toolbox.inventory import *
 from toolbox.webscraper import *
 from toolbox.imageRecognition import *
+from toolbox.marketManager import *
+import threading as mp
 #from toolbox.marketManager import *
-#required python modules: pynput, (lxml, requests, cssselect,)<-webscrape (oauth2client, PyOpenSSL, gspread)<-google spreadsheets
-
+#required python modules: pynput, (lxml, requests, cssselect,)<-webscrape, pandas
+#uninstall (no longer needed): (oauth2client, PyOpenSSL, gspread)<-google spreadsheets
 #print(dissectTrade(readMessage()[1]))
+"""
+def testProcess():
+    i = 1
+    while( i < 5):
+        time.sleep(1)
+        print(i)
+        i+=1
+"""
+def chatReadLoop():
+    while True:
+        time.sleep(1)
+        print(readMessage())
+
+#def marketReadLoop():
+if __name__ == "__main__":
+    chatRead = mp.Thread(target=chatReadLoop)
+    #test = mp.Thread(target=testProcess)
+
+    chatRead.start()
+    #test.start()
+    chatRead.join()
+    #test.join()
--- main/toolbox/marketManager.py
+++ main/toolbox/marketManager.py
@@ -1,19 +1,37 @@
 #from toolbox.general import *
 #from toolbox.webscraper import *
-'''
-import json
-import gspread
-from oauth2client.client import SignedJwtAssertionCredentials
+import pandas
+itemID = {
+    1 : 'alteration',
+    2 : 'fusing',
+    3 : 'alchemy',
+    4 : 'chaos',
+    5 : 'gemcutters',
+    6 : 'exalted',
+    7 : 'chroma',
+    8 : 'jewellers',
+    9 : 'chance',
+    10 : 'chisel',
+    11 : 'scour',
+    12 : 'blessed',
+    13 : 'regret',
+    14 : 'regal',
+    15 : 'divine',
+    16 : 'vaal',
+    17 : 'wisdom scrolls',
+    18 : 'portal scrolls',
+    19 : 'armour',
+    20 : 'whetstone',
+    21 : 'glassblowers',
+    22 : 'transmutes',
+    23 : 'augmentation',
+    24 : 'mirror',
+}
 
-json_key = json.load(open('POEflipBot-5ef914e19a0d.json')) # json credentials you downloaded earlier
-scope = ['https://spreadsheets.google.com/feeds']
+currencyToBuy = itemID[4]
+currencyToSell = itemID[9]
 
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope) # get email and key from creds
 
-file = gspread.authorize(credentials) # authenticate with Google
-sheet = file.open("POE Market Data").sheet1 # open sheet
 
-all_cells = sheet.range('A1:C6')
-for cell in all_cells:
-    print (cell.value)
-'''
+#def findBestFlip():
+    #all kinds of math will go in here & will use webscraper functions
